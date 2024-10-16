01b5a6139baff739d02e847c27f9c590b78d4af9
Martin Lindbüchl <lblsolutions@outlook.de>
2024-10-16 15:57:06 +0200

    Create fix_clangd.sh

---
+++ fix_clangd.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+if [ ! -f "$1/CMakeLists.txt" ]; then
+    echo "Error: $1 does not contain a CMakeLists.txt file."
+    read -r -p "Do you want to create a template CMakeLists.txt file? (y/n) " create_template
+    if [ "$create_template" == "y" ]; then
+        cp ./cmake-template "$1/CMakeLists.txt"
+        echo "int main(int argc, char **argv) { return 0; }" > "$1/template.cpp"
+        echo "Template CMakeLists.txt file created in $1."
+    else
+        exit 1
+    fi
+fi
+
+mkdir -p "$1/build"
+cd "$1/build" || exit 1
+
+# Run CMake and generate the compile_commands.json file
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+# Patch compile_commands.json
+sed -i 's/\\\"//g' compile_commands.json
